6381180c0af714d2de68d7bf4eb02deaa4dd80dd
a2dhotre <akdhotre@in.ibm.com>
2018-01-07 22:58:21 +0530

    'first_commit'

---
+++ django_level_five/learning_users/basic_app/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from basic_app.forms import UserForm,UserPofileInfoForm
+
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login , logout
+
+# Create your views here.
+
+def index(request):
+    return render(request,'basic_app/index.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+@login_required
+def special(request):
+    HttpResponse("you are loggd in!")
+
+def register(request):
+
+    registerd = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserPofileInfoForm(data = request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit = False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+            profile.save()
+
+            registerd = True
+
+        else:
+                print(user_form.errors,profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserPofileInfoForm()
+
+    return render(request,'basic_app/registration.html',
+                            {'user_form':user_form,
+                             'profile_form':profile_form,
+                             'registerd':registerd})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Account not active!")
+        else:
+            print('Someone tried to login and faild!')
+            print("Username: {} and password {}".format(username,password))
+            return HttpResponse("Invalid Login details!")
+    else:
+        return render(request,'basic_app/login.html')
